d4c73ad525f938fd6b4669a3f4de5a1fb4254b3a
Martijn Braam <martijn@brixit.nl>
2015-12-17 11:17:11 +0100

    Added splash image

---
+++ Tools/png2array.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import struct
+import sys
+
+canvas = Image.open(sys.argv[1])
+canvas.load()
+output = []
+
+for y in range(0, int(canvas.size[1]/8)):
+    for x in range(0, canvas.size[0]):
+        output_byte = ''
+        for p in range(0,8):
+            pixel = canvas.getpixel((x,(y*8)+p))
+            output_byte=str(pixel)+output_byte
+        output.append("0x%x" % int(output_byte,2))
+
+print(", ".join(output))
